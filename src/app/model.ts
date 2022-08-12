d6e142aa87d5b11939323d7920b5c4ec8b11c6ee
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-08-12 11:45:25 +0300

    Add files via upload

---
+++ src/app/model.ts
@@ -0,0 +1,13 @@
+import { TodoItem } from "./todoitem"
+
+export class Model{
+    name:string
+    items: TodoItem[];
+
+    constructor(){
+        this.name = "Arda";
+        this.items=[
+
+        ];
+    }
+}
